c2a5da1ceab193571a6431223f0118e40dd1eb90
Екатерина Мужжухина <awesome-170693@yandex.ru>
2023-03-02 21:50:39 +0300

    Add csrf_failure as handler403

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
 
 handler404 = 'core.views.page_not_found'
 
+handler403 = 'core.views.csrf_failure'
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
